a516cff9112d2ffb4943b143084a5ddc9d3c4937
Rohan Bagchi <rohan@recruiterbox.com>
2017-07-17 20:59:23 +0530

    Remove unused files; add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        Hello World
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
